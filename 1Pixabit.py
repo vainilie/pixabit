146f117dc50ef6448aa316dcb1eb771abb2d3a09
vainilie <vainilie@outlook.com>
2023-07-30 02:01:38 -0600

    .

--- 1Pixabit.py
+++ 1Pixabit.py
@@ -4,6 +4,7 @@ import get_stats
 import get_tags
 import clean_tags
 import get_tasks
+import Rich
 from rich.theme import Theme
 from rich.console import Console
 
@@ -17,7 +18,7 @@ all_tasks = get_tasks.process_tasks(all_tags)
 stats = get_stats.get_stats(all_tasks)
 unused_tags = clean_tags.get_unused_tags(all_tags, all_tasks["tags"])
 
-# Rich.display(Stats)
+Rich.print_stats(stats)
 # # if Stats["sleeping"] is True:
 # #     sleeping_status.sleeping_status("sleeping", "awake")
 # # else:
--- Rich.py
+++ Rich.py
@@ -1,105 +1,3 @@
-# from rich.table import Table
-# import timeago
-# from rich.console import Console, ConsoleOptions, Group, RenderableType, RenderResult
-# from rich.panel import Panel
-# from rich.prompt import Confirm, Prompt, IntPrompt
-# from art import text2art
-# from datetime import datetime, timezone
-# from rich.theme import Theme
-# import timeago, datetime
-
-# from rich import box
-
-# theme = Theme.read("styles")
-
-# console = Console(theme=theme)
-# # nowUTC = datetime.now(timezone.utc)
-# # nowLOC = nowUTC.astimezone()
-
-# #
-# # ─── % DISPLAY STATS ────────────────────────────────────────────────────────────
-# #
-
-
-# def display(Stats):
-#     """display"""
-
-#     now = datetime.datetime.now(datetime.timezone.utc)
-#     LastLogin = Stats.get("time")
-#     LastLogin = datetime.datetime.fromisoformat(LastLogin)
-
-#     userStats = Table.grid(padding=(0, 2), expand=True)
-#     userStats.add_column(no_wrap=True, justify="right")
-#     userStats.add_column(no_wrap=True, justify="left")
-
-#     userStats.add_row(
-#         "[habitica][b]",
-#         f"[habitica]Hello, [b i]{Stats.get('username')}",
-#     )
-#     userStats.add_row(
-#         "[neo][b]󰔚",
-#         f"You last logged-in [b i]{timeago.format(LastLogin, now)}",
-#     )
-#     userStats.add_row(
-#         "[b #2995CD]start time down",
-#         f"{Stats.get('start')} am",
-#     )
-#     if Stats.get("sleeping") is True:
-#         userStats.add_row("[gold][b]󰒲", "You are [gold][i b]resting[/] in the Inn")
-
-#     if bool(Stats.get("quest")) is True:
-#         quest = Table.grid(
-#             expand=False,
-#         )
-#         quest.add_column()
-#         quest.add_column()
-#         quest.add_column()
-#         quest.add_row(
-#             "[pink]You're [i]in a quest",
-#             "",
-#             f"[up][b] 󰶣 {int(Stats.get('quest').get('progress').get('up'))}",
-#             f"[down][b] 󰶡 {int(Stats.get('quest').get('progress').get('down'))}",
-#         )
-#         userStats.add_row("[b][pink]󰞇", quest)
-
-#     stats = Table.grid(padding=(0, 2), expand=True)
-
-#     stats.add_row("[b #FFA624]󱉏", f"{int(Stats.get('stats').get('gp'))}")
-
-#     stats.add_row("[b #F74E52]", f"{int(Stats.get('stats').get('hp'))}")
-
-#     stats.add_row("[b #FFBE5D]󰫢", f"{int(Stats.get('stats').get('exp'))}")
-
-#     stats.add_row("[b #50B5E9]", f"{int(Stats.get('stats').get('mp'))}")
-#     about = Table.grid(padding=0, expand=True)
-#     about.add_column(no_wrap=True)
-#     about.add_row("[b #6133b4]" + text2art("HABITICA", font="eftifont"))
-#     about.add_row(userStats)
-#     aboute = Table.grid(padding=2, expand=True)
-#     aboute.add_column(no_wrap=True)
-#     aboute.add_column(no_wrap=True)
-#     statss = Panel(
-#         stats,
-#         box=box.ROUNDED,
-#         title=f"[b][i]level {Stats.get('level')}:sparkles:",
-#         border_style="pink",
-#         subtitle=f"{Stats.get('class')}",
-#         expand=False,
-#     )
-
-#     aboute.add_row(about, statss)
-
-#     console.print(
-#         Panel(
-#             aboute,
-#             box=box.ROUNDED,
-#             title=f":space_invader: [b]{text2art('Habitica Stats', font='fancy135')} :space_invader:",
-#             border_style="#BDA8FF",
-#             expand=False,
-#         ),
-#     )
-#     return Stats
-
 from rich.table import Table
 from rich.console import Console
 from rich.panel import Panel
@@ -114,37 +12,37 @@ theme = Theme.read("styles")
 console = Console(theme=theme)
 
 
-def display(Stats):
+def print_stats(input_stats):
     """Display user stats in a rich panel."""
 
     now = datetime.datetime.now(datetime.timezone.utc)
-    LastLogin = Stats.get("time")
+    LastLogin = input_stats.get("time")
     LastLogin = datetime.datetime.fromisoformat(LastLogin)
 
     # Create a table to show user information
-    userStats = Table.grid(padding=(0, 2), expand=True)
-    userStats.add_column(no_wrap=True, justify="right")
-    userStats.add_column(no_wrap=True, justify="left")
+    user_stats = Table.grid(padding=(0, 2), expand=True)
+    user_stats.add_column(no_wrap=True, justify="right")
+    user_stats.add_column(no_wrap=True, justify="left")
 
-    userStats.add_row(
+    user_stats.add_row(
         "[habitica][b]",
-        f"[habitica]Hello, [b i]{Stats.get('username')}",
+        f"[habitica]Hello, [b i]{input_stats.get('username')}",
     )
-    userStats.add_row(
+    user_stats.add_row(
         "[neo][b]󰔚",
         f"You last logged-in [b i]{timeago.format(LastLogin, now)}",
     )
-    userStats.add_row(
-        "[b #2995CD]🕧",
-        f"Your day starts at {Stats.get('start')} am",
+    user_stats.add_row(
+        "[b #2995CD]⧗",
+        f"Your day starts at {input_stats.get('start')} am",
     )
 
     # Add the 'resting in the Inn' status if user is sleeping
-    if Stats.get("sleeping") is True:
-        userStats.add_row("[gold][b]󰒲", "You are [gold][i b]resting[/] in the Inn")
+    if input_stats.get("sleeping") is True:
+        user_stats.add_row("[gold][b]󰒲", "You are [gold][i b]resting[/] in the Inn")
 
     # Add the 'in a quest' information if user is in a quest
-    if bool(Stats.get("quest")) is True:
+    if bool(input_stats.get("quest")) is True:
         quest = Table.grid(expand=False)
         quest.add_column()
         quest.add_column()
@@ -152,51 +50,90 @@ def display(Stats):
         quest.add_row(
             "[pink]You're [i]in a quest",
             "",
-            f"[up][b] 󰶣 {int(Stats.get('quest').get('progress').get('up'))}",
-            f"[down][b] 󰶡 {int(Stats.get('quest').get('progress').get('down'))}",
+            f"[up][b] 󰶣 {int(input_stats.get('quest').get('progress').get('up'))}",
+            f"[down][b] 󰶡 {int(input_stats.get('quest').get('progress').get('down'))}",
         )
-        userStats.add_row("[b][pink]󰞇", quest)
+        user_stats.add_row("[b][pink]󰞇", quest)
 
     # Create a table to show user stats
     stats = Table.grid(padding=(0, 2), expand=True)
 
     # Add rows for each stat
-    stats.add_row("[b #FFA624]󱉏", f"{int(Stats.get('stats').get('gp'))}")
-    stats.add_row("[b #F74E52]", f"{int(Stats.get('stats').get('hp'))}")
-    stats.add_row("[b #FFBE5D]󰫢", f"{int(Stats.get('stats').get('exp'))}")
-    stats.add_row("[b #50B5E9]", f"{int(Stats.get('stats').get('mp'))}")
+    stats.add_row("[b #FFA624]󱉏", f"{int(input_stats.get('stats').get('gp'))}")
+    stats.add_row("[b #F74E52]", f"{int(input_stats.get('stats').get('hp'))}")
+    stats.add_row("[b #FFBE5D]󰫢", f"{int(input_stats.get('stats').get('exp'))}")
+    stats.add_row("[b #50B5E9]", f"{int(input_stats.get('stats').get('mp'))}")
+
+    # Create a table to show task counts
+    counts = Table.grid(padding=(0, 2), expand=True)
+
+    # Add rows for each stat
+    counts.add_row(
+        "[b #FFA624]Dailys:", f"{sum(input_stats['numbers']['dailys'].values())}"
+    )
+    counts.add_row("[b #FFA624]Habits:", f"{input_stats['numbers']['habits']}")
+    counts.add_row(
+        "[b #FFA624]Todos:", f"{sum(input_stats['numbers']['todos'].values())}"
+    )
+    counts.add_row("[b #FFA624]Rewards:", f"{input_stats['numbers']['rewards']}")
+    counts.add_row()
+
+    counts.add_row("[b #FFA624]❑ dailys:", f"{input_stats['numbers']['dailys']['due']}")
+    counts.add_row(
+        "[b #FFA624]◯ dailys:", f"{input_stats['numbers']['dailys']['grey']}"
+    )
+    counts.add_row(
+        "[b #FFA624]:heavy_check_mark: dailys:",
+        f"{input_stats['numbers']['dailys']['done']}",
+    )
+    counts.add_row()
+
+    counts.add_row("[b #FFA624]❑ todos:", f"{input_stats['numbers']['todos']['due']}")
+    counts.add_row("[b #FFA624]☒ todos:", f"{input_stats['numbers']['todos']['red']}")
+    counts.add_row("[b #FFA624]◯ todos:", f"{input_stats['numbers']['todos']['grey']}")
 
     # Create a table for the "About" section
     about = Table.grid(padding=0, expand=True)
     about.add_column(no_wrap=True)
     about.add_row("[b #6133b4]" + text2art("HABITICA", font="eftifont"))
-    about.add_row(userStats)
+    about.add_row(user_stats)
 
     # Create a panel to display user stats and info
-    statss = Panel(
+    display_stats = Panel(
         stats,
         box=box.ROUNDED,
-        title=f"[b][i]level {Stats.get('level')}:sparkles:",
+        title=f"[b][i]level {input_stats.get('level')}:sparkles:",
         border_style="pink",
-        subtitle=f"{Stats.get('class')}",
-        expand=False,
+        subtitle=f"{input_stats.get('class')}",
+        expand=True,
     )
 
+    # Create a panel to display user stats and info
+    display_numbers = Panel(
+        counts,
+        box=box.ROUNDED,
+        title=f"[b][i]total tasks: {input_stats['numbers']['total']} :sparkles:",
+        border_style="pink",
+        subtitle=f"{input_stats.get('class')}",
+        expand=True,
+    )
     # Create a table for the final display
-    aboute = Table.grid(padding=2, expand=True)
-    aboute.add_column(no_wrap=True)
-    aboute.add_column(no_wrap=True)
-    aboute.add_row(about, statss)
+    about_panel = Table.grid(padding=2, expand=False)
+    about_panel.add_column(no_wrap=True)
+    about_panel.add_column(no_wrap=True)
+    about_panel.add_column(no_wrap=True)
+
+    about_panel.add_row(about, display_stats, display_numbers)
 
     # Print the final panel with the user stats
     console.print(
         Panel(
-            aboute,
+            about_panel,
             box=box.ROUNDED,
-            title=f":space_invader: [b]{text2art('Habitica Stats', font='fancy135')} :space_invader:",
+            title=":space_invader: [b i]Stats :space_invader:",
             border_style="#BDA8FF",
             expand=False,
         ),
     )
 
-    return Stats
+    return input_stats
