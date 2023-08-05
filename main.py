8b39e378102d1cdaf21821b485fd2500e2dd924e
vainilie <vainilie@outlook.com>
2023-08-05 01:01:36 -0600

    :D

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
-from core import auth_file, habitica_api
-from core import save_file
-from get import get_tags, get_tasks, get_stats
+from core import auth_file, save_file
+from get import get_tags, get_tasks, get_stats, get_userdata
 from interface import Rich, rich_tags
-from actions import unused_tags, filter_task
+from actions import unused_tags, filter_task, sleeping, category_tags
+from utils.rich_utils import print, Confirm, IntPrompt, Prompt, console
+
 auth_file.check_auth_file()
 tags = get_tags.get_tags()
 all_tasks = get_tasks.process_tasks(tags)
 stats = get_stats.get_user_stats(all_tasks["cats"])
-
-
 unused = unused_tags.get_unused_tags(tags, all_tasks["cats"]["tags"])
-unused_tags.delete_unused_tags(unused)
-#save_user_data.save_all_user_data()
-Rich.print_stats(stats)
-#rich_tags.print_tags(all_tags)
-rich_tags.print_unused(unused)
-filter_task.find_broken(all_tasks["data"], all_tasks["cats"]["broken"])
-filter_task.list_broken(all_tasks["data"], all_tasks["cats"]["broken"])
 
-# # if Stats["sleeping"] is True:
-# #     sleeping_status.sleeping_status("sleeping", "awake")
-# # else:
-# #     sleeping_status.sleeping_status("awake", "sleeping")
-# rich_tags.show_tags(Tagsx, "name")
+
+if Confirm.ask("Print [i]stats[/i]?", default=True):
+    if True:
+        Rich.print_stats(stats)
+if Confirm.ask("Print [i]unused tags[/i]?", default=True):
+    if True:
+        rich_tags.print_unused(unused)
+        if Confirm.ask("Delete [i]unused tags[/i]?", default=False):
+            if True:
+                unused_tags.delete_unused_tags(unused)
+category_tags.category_Tags(all_tasks["data"])
+fruit = Prompt.ask("Enter a fruit", choices=["save", "tags", "broken"])
+print(f"Selected {fruit!r}")
+if fruit == "save":
+    get_userdata.save_all_user_data()
+elif fruit == "tags":
+    rich_tags.print_tags(tags)
+elif fruit == "broken":
+    if stats["broken"] > 0:
+        filter_task.list_broken(all_tasks["data"], all_tasks["cats"]["broken"])
+        if Confirm.ask("Unlink [i]broken tasks[/i]?", default=False):
+            if True:
+                filter_task.delete_broken(all_tasks["data"], all_tasks["cats"]["broken"])
+    else:
+        print("No broken tasks")
+elif fruit == "sleep":
+    if stats["sleeping"] is True:
+        sleeping.toggle_sleeping_status("sleeping", "awake")
+    else:
+        sleeping.toggle_sleeping_status("awake", "sleeping")
+
+else:
+    print("[b]OK :loudly_crying_face:")
