d0b41c12ab9c3a693665bdaa3434f9c4ed5805cc
vainilie <vainilie@outlook.com>
2023-08-11 00:22:45 -0600

    Added sort alphanumeric

---
+++ actions/sort.py
@@ -0,0 +1,11 @@
+from core import habitica_api
+from get import get_rawtasks
+import time
+
+
+def sort_alpha():
+    tasks = get_rawtasks.get_rawtasks()
+    for idx, task in enumerate(tasks):
+        habitica_api.post(f"tasks/{task['_id']}/move/to/-1")
+        print(idx, len(tasks), "/")
+        time.sleep(60 / 30)
--- get/get_rawtasks.py
+++ get/get_rawtasks.py
@@ -1,7 +1,12 @@
 from core import habitica_api, save_file
+import emoji_data_python
 
 
 def get_rawtasks():
     tasks = habitica_api.get("tasks/user")["data"]
-    save_file.save_file(tasks, "raw_tasks", "_json")
+    for task in tasks:
+        emoji_data_python.replace_colons(task["text"])
+
+    sorted_tasks = sorted(tasks, key=lambda x: x["text"].lower())
+    save_file.save_file(sorted_tasks, "raw_tasks", "_json")
     return tasks
