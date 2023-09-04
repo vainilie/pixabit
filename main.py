bbc61fbbce8da8d39d4fa95fe9027b6dcf38af59
vainilie <vainilie@outlook.com>
2023-09-04 02:04:37 -0600

    :

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from utils.rich_utils import Confirm, IntPrompt, Panel, Columns, console
 from core.auth_file import check_auth_file
 from get import bkup_challenges, get_tags, get_tasks, get_stats, get_userdata
 from interface import rich_stats, rich_tags, mainmenu
-from actions import unused_tags, filter_task, sleeping, category_tags, challenges
+from actions import unused_tags, filter_task, sleeping, category_tags, challenges, sort
 
 def main():
     while True:
+#        sort.sort_alpha()
         selected_action = mainmenu.display_main_menu()
         mainmenu.select_option(selected_action)
 
