217edf92d22ffff81fa0aeaa5700f804f6802e83
vainilie <vainilie@outlook.com>
2024-03-11 02:51:58 -0600

    Multitag

--- interface/rich_tags.py
+++ interface/rich_tags.py
@@ -2,18 +2,20 @@ from utils.rich_utils import Table, Console, Panel, box, console, Columns
 from interface import rich_chtag
 
 def print_tags(tags):
-    for category in tags:
-        tag_renderables = [
-            f"[b]{num}.[/] [i]{tag['name']}"
-            for num, tag in enumerate(tags[category], start=1)
-        ]
-        tag_render = Panel(
-            Columns(tag_renderables, column_first=True, padding=(0, 4)),
-            expand=False,
-            width=50,
-            title=f"[gold][i]{category}",
-        )
-        console.print(tag_render)
+    # num=1
+    # for category in tags:
+    #     tag_renderables = [
+    #         f"[b]{num}.[/] [i]{tag['name'][:24]}" 
+    #         for num, tag in enumerate(tags[category], start=num)
+    #     ]
+    #     tag_render = Panel(
+    #         Columns(tag_renderables, column_first=True, padding=(0, 4)            ,
+    #         title=f"[gold][i]{category}",
+    #         expand=False)
+    #     )
+    #     console.print(tag_render,overflow=Ellipsis)
+    #     num += len(tag_renderables)
+
     rich_chtag.print_tags(tags)
 
 
@@ -24,8 +26,8 @@ def print_unused(tags):
         for num, tag in enumerate(tags, start=1)
     ]
     tag_render = Panel(
-        Columns(tag_renderables, column_first=True, padding=(0, 4)),
-        expand=True,
+        Columns(tag_renderables, column_first=True, padding=(0, 4),        expand=True,
+),
         title="[gold][i]Unused Tags",
     )
     console.print(tag_render)
