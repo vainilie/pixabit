eedfdc4f4454039b8c9062f6b570ab0fe5a1b915
vainilie <vainilie@outlook.com>
2025-01-12 17:20:05 -0600

    Bckup!

--- actions/challenges.py
+++ actions/challenges.py
@@ -7,11 +7,12 @@ from utils.rich_utils import Table, Console, Panel, box, console, Columns
 
 ### api get challenges member YES not working
 
+
 def list_challenges(challengue):
     all_challenges = get_challenges.get_my_challenges()
-    
-    challenges_table=  Table(title="Challenges",box=box.MARKDOWN)
-    challenges_table.add_column("#")   
+
+    challenges_table = Table(title="Challenges", box=box.MARKDOWN)
+    challenges_table.add_column("#")
     challenges_table.add_column("$")
     challenges_table.add_column("u")
     challenges_table.add_column("%")
@@ -23,14 +24,21 @@ def list_challenges(challengue):
     challenges_table.add_column("name")
     for idx, challenge in enumerate(all_challenges):
         if challenge["id"] in challengue["cats"]["challenge"]:
-            challenges_table.add_row(str(idx), str(challenge['prize']), 
-                                     str(challenge['memberCount']),
-                                     str(round(100/challenge['memberCount'],2)),
-                                     str(len(challenge['tasksOrder']['habits'])),
-                                     str(len(challenge['tasksOrder']['dailys'])),
-                                     str(len(challenge['tasksOrder']['todos'])),
-                                     str(len(challenge['tasksOrder']['rewards'])),
-str(len(challenge['tasksOrder']['habits']) +                len(challenge['tasksOrder']['dailys']) +                len(challenge['tasksOrder']['todos']) +                len(challenge['tasksOrder']['rewards'])),
-challenge['name'][:25]
-        )
+            challenges_table.add_row(
+                str(idx),
+                str(challenge["prize"]),
+                str(challenge["memberCount"]),
+                str(round(100 / challenge["memberCount"], 2)),
+                str(len(challenge["tasksOrder"]["habits"])),
+                str(len(challenge["tasksOrder"]["dailys"])),
+                str(len(challenge["tasksOrder"]["todos"])),
+                str(len(challenge["tasksOrder"]["rewards"])),
+                str(
+                    len(challenge["tasksOrder"]["habits"])
+                    + len(challenge["tasksOrder"]["dailys"])
+                    + len(challenge["tasksOrder"]["todos"])
+                    + len(challenge["tasksOrder"]["rewards"])
+                ),
+                challenge["name"][:25],
+            )
     print(challenges_table)
